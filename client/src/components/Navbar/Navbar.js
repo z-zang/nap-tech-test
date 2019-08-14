3776896c0abe6cc45f39e53a19a66f2e6cb3e77c
Zichao Zang <zichao.zang@ynap.com>
2019-08-14 22:15:00 +0100

    managed to get state working properly, turns out useEffect needs 2nd param of var that when changed should make the code update. next: add ability to filter by designers; <select> will need state and clientside fetchProducts needs rewriting (may also need to combine two routes on server side to be able to filter by designers and also sort at the same time

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,49 +1,40 @@
 import React, {useState, useEffect} from 'react';
 
-function Navbar({offset, setOffset, fetchProducts}) {
+function Navbar({offset, setOffset}) {
   const [designers, setDesigners] = useState([])
-  console.log("offset from navbar", offset)
 
   // fetch designers
   async function fetchDesignerList() {
     var res = await fetch('/api/designers')
     var response = await res.json()
-    console.log("response", response.uniqDesArr, typeof response)
     setDesigners(response.uniqDesArr)
   }
 
   useEffect(() => {
     fetchDesignerList()
-    console.log(designers)
   }, [])
 
   function incrOffset() {
-    offset += 60
-    setOffset(offset)
-    // console.log('incrOffset')
-    fetchProducts()
+    var temp = offset + 60
+    setOffset(temp)
   }
-
   function decrOffset() {
-    offset -= 60
-    setOffset(offset)
-    // console.log('decrOffset')
-    fetchProducts()
+    var temp = offset - 60
+    setOffset(temp)
   }
 
   return (
     <nav>
       <select>
-        <option value="" defaultValue disabled>Sort By</option>
+        <option value="Sort By">Sort By</option>
         <option value="Price low to high">Price low to high</option>
         <option value="Price high to low">Price high to low</option>
-        <option value="Designer">Designer A-Z</option>
       </select>
 
       <select>
-        <option value="" defaultValue disabled>Choose Designer</option>
+        <option value="Choose Designer" defaultValue>Choose Designer</option>
         {designers.map(el =>
-          <option value={el}>{el}</option>
+          <option value={el} key={designers[el]}>{el}</option>
         )}
 
         <option value="Designer">Designer</option>
