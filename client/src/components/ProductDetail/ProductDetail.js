d79ff20173cbe8ae639cce7d2ea5962b9cf8bbe7
Zichao Zang <zichao.zang@ynap.com>
2019-08-16 23:00:50 +0100

    got the productdetail images to load properly, added start script in json, app.css is wip for productdetail styling

--- client/src/components/ProductDetail/ProductDetail.js
+++ client/src/components/ProductDetail/ProductDetail.js
@@ -11,42 +11,40 @@ function ProductDetail({match}) {
     console.log('fetchPid requestURL:', url)
     var res = await fetch(url)
     var response = await res.json()
-    console.log(response)
     setProdData(response)
   }
 
   useEffect(() => {
-
     fetchPid()
-  }, [pid])
+  }, [])
 
   function goBack() {
     window.history.back()
-
   }
 
-
-
   return (
+
     <div>
       <div className="product-detail">
-        {/* <p>{prodData[0]}</p> */}
-        {/* <p>{prodData[id]}</p> */}
-
-          <p>{prodData.id}</p>
           
           <img src={prodData.images === undefined ? '' : prodData.images.large }/>
           <p>{prodData.name}</p>
           <p>{prodData.designer}</p>
           <p>{prodData.price}</p>
-          {/* <img src={prodData.images.outfit}/> */}
-          <img src={prodData.images === undefined ? '' : prodData.images.outfit }/>
-          <img src={prodData.images === undefined ? '' : prodData.images.small }/>
-          
+
+          {
+
+            !prodData.images ? null : 
+            <div className="prod-detail-imgs">
+              <img src={prodData.images.outfit} alt={`${prodData.name} outfit`}/>
+              <img src={prodData.images.small} alt={`${prodData.name} small`}/>
+
+            </div>
+
+          }
+          <p>{prodData.id}</p>
           {prodData.onSale === true ? <p>on Sale</p> : ''}
 
-
-        {/* <p>{prodData.designer}</p> */}
         <Link>
           <button onClick={goBack}>Back to products</button>
         </Link>
