a13bf4a7b8299957123ab3c441d77b6283b593f0
Zichao Zang <zichao.zang@ynap.com>
2019-08-17 21:49:59 +0100

    split css into different files, refactored into es6 and better scoped variables, updated show numbers for results, added better styling for the ProductDetail component

--- client/src/components/ProductDetail/ProductDetail.js
+++ client/src/components/ProductDetail/ProductDetail.js
@@ -1,16 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
+import './ProductDetail.css'
 
 function ProductDetail({match}) {
   const [prodData, setProdData] = useState({})
-  var pid = match.params.pid
+  let pid = match.params.pid
 
   // fetch by PID
-  async function fetchPid() {
-    var url = '/api/product/' + pid
+  const fetchPid = async () => {
+    let url = '/api/product/' + pid
     console.log('fetchPid requestURL:', url)
-    var res = await fetch(url)
-    var response = await res.json()
+    let res = await fetch(url)
+    let response = await res.json()
     setProdData(response)
   }
 
@@ -18,36 +19,46 @@ function ProductDetail({match}) {
     fetchPid()
   }, [])
 
-  function goBack() {
+  const goBack = () => {
     window.history.back()
   }
 
+  if (Object.getOwnPropertyNames(prodData).length === 0) {
+    return (
+      <div>Loading..</div>
+    )
+  }
+
   return (
-
     <div>
-      <div className="product-detail">
-          
-          <img src={prodData.images === undefined ? '' : prodData.images.large }/>
-          <p>{prodData.name}</p>
-          <p>{prodData.designer}</p>
-          <p>{prodData.price}</p>
-
-          {
-
-            !prodData.images ? null : 
-            <div className="prod-detail-imgs">
-              <img src={prodData.images.outfit} alt={`${prodData.name} outfit`}/>
-              <img src={prodData.images.small} alt={`${prodData.name} small`}/>
-
-            </div>
-
-          }
-          <p>{prodData.id}</p>
-          {prodData.onSale === true ? <p>on Sale</p> : ''}
-
-        <Link>
-          <button onClick={goBack}>Back to products</button>
+      <div className="product-detail">  
+        <Link to='/'>
+          <button className='back-button' onClick={goBack}>⬅ Back to products</button>
         </Link>
+
+          <h2>{prodData.designer}</h2>
+          <h4>{prodData.name}</h4>
+          <p>£{prodData.price}</p>
+
+          <img src={prodData.images.large} alt={`${prodData.name} large image`} className='image-large'/>
+
+          <div className="prod-detail-imgs">
+            <img src={prodData.images.outfit} alt={`${prodData.name} outfit image`}/>
+            <img src={prodData.images.small} alt={`${prodData.name} small image`}/>
+          </div>
+
+          <p><i>Product id: {prodData.id}</i></p>
+
+          {prodData.onSale === true ? <p>Onsale</p> : ''}
+          
+          { prodData.badges.map(el => 
+          <p>Badge: {el}</p>)}
+          <div>
+            {prodData.sizes.map(el => 
+            <button>Size: {el.name}</button>
+            )}
+          </div>
+
       </div>
     </div>
 
